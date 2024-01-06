88bbfdf9c623739efa7498ff887f9bd43de05101
Maksim Kornakov <makskornakov@ya.ru>
2024-01-06 10:44:25 +0100

    tile collision

--- src/ECS/ColliderComponent.h
+++ src/ECS/ColliderComponent.h
@@ -23,6 +23,8 @@ public:
       entity->addComponent<TransformComponent>();
     }
     transform = &entity->getComponent<TransformComponent>();
+
+    Game::colliders.push_back(this);
   }
 
   void update() override
--- src/Game.cpp
+++ src/Game.cpp
@@ -11,8 +11,14 @@ Manager manager;
 SDL_Renderer *Game::renderer = nullptr;
 SDL_Event Game::event;
 
+std::vector<ColliderComponent *> Game::colliders;
+
 auto &newPlayer(manager.addEntity());
 auto &wall(manager.addEntity());
+
+auto &tile0(manager.addEntity());
+auto &tile1(manager.addEntity());
+auto &tile2(manager.addEntity());
 
 Game::Game()
 {
@@ -45,6 +51,15 @@ void Game::init(const char *title, int width, int height, bool fullScreen)
 
   map = new Map();
 
+  // ECS implementation
+
+  tile0.addComponent<TileComponent>(200, 200, 32, 32, 0);
+  tile0.addComponent<ColliderComponent>("water");
+  tile1.addComponent<TileComponent>(250, 250, 32, 32, 1);
+  tile1.addComponent<ColliderComponent>("dirt");
+  tile2.addComponent<TileComponent>(300, 300, 32, 32, 2);
+  tile2.addComponent<ColliderComponent>("grass");
+
   wall.addComponent<TransformComponent>(300.0f, 300.0f, 300, 20, 1);
   wall.addComponent<SpriteComponent>("../Resources/tiles/dirt.png");
   wall.addComponent<ColliderComponent>("wall");
@@ -76,17 +91,22 @@ void Game::update()
   manager.refresh();
   manager.update();
 
-  if (Collision::AABB(newPlayer.getComponent<ColliderComponent>().collider, wall.getComponent<ColliderComponent>().collider))
+  // if (Collision::AABB(newPlayer.getComponent<ColliderComponent>().collider, wall.getComponent<ColliderComponent>().collider))
+  // {
+  //   newPlayer.getComponent<TransformComponent>().velocity * -1;
+  //   std::cout << "player hit wall" << std::endl;
+  // }
+
+  for (auto cc : colliders)
   {
-    // newPlayer.getComponent<TransformComponent>().scale = 1;
-    std::cout << "player hit wall" << std::endl;
+    Collision::AABB(newPlayer.getComponent<ColliderComponent>(), *cc);
   }
 }
 
 void Game::render()
 {
   SDL_RenderClear(renderer);
-  map->DrawMap();
+  // map->DrawMap();
 
   manager.draw();
 
